9a0b16126d7171e70dcfa7043252590914ff25bc
care <eishatanveer28@gmail.com>
2021-06-07 01:06:10 -0700

    this is my first commit

--- app/src/main/java/com/example/mc_lab10/MainActivity.java
+++ app/src/main/java/com/example/mc_lab10/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.mc_lab10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Spinner spinner;
+    ArrayList<Object> list= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.tv_1);
+        spinner=findViewById(R.id.spin_1);
+        list.add(19);
+        list.add(20);
+        list.add(21);
+        list.add(22);
+
+        ArrayAdapter<int>
+
+
     }
 }
