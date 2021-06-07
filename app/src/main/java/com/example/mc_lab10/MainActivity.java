fff04bbd4b542f978193c5a621f0a28882e3e7b3
care <eishatanveer28@gmail.com>
2021-06-07 01:00:33 -0700

    this is my first commit

---
+++ app/src/main/java/com/example/mc_lab10/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mc_lab10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.tv_1);
+    }
+}
